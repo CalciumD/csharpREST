bcf8d5f887aa545c18e0cc959cd70f9a1d3716d6
Callum :D <callumackery95@gmail.com>
2023-09-11 18:52:07 +0100

    latest

---
+++ csharpREST/Controllers/ShwmaeWorld.cs
@@ -0,0 +1,86 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace csharpREST.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ShwmaeWorld : ControllerBase
+    {
+        
+        private readonly List<User> _users;
+
+        public ShwmaeWorld()
+        {
+            _users = new List<User>()
+            {
+                new User() { Id =  1, Name = "Bob"},
+                new User() { Id = 2, Name = "John"},
+                new User() { Id = 3, Name = "Derek"}
+            };
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetUsers(int id)
+        {
+            return _users;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUser(int id)
+        {
+            var user = _users[id];
+            if(user != null)
+            {
+                return user;
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult<User> AddUser([FromBody] User user)
+        {
+            user.Id = GenerateNewId();
+            _users.Add(user);
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+
+        private int GenerateNewId()
+        {
+            return _users.Count + 1;
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(int id, [FromBody] User updatedUser)
+        {
+            var user = _users.Find(x => x.Id == id);
+            if(user != null)
+            {
+                user.Name = updatedUser.Name;
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(int id)
+        {
+            var user = _users.Find(u => u.Id == id);
+            if( user != null )
+            {
+                _users.Remove(user);
+                return NoContent();
+            }else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
